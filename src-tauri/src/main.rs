ac916a825b69449b0007e10fd8ac3bed73d8b399
overoxidize <overoxidize@protonmail.com>
2023-01-29 07:35:26 -0500

    adding traits and dependencies for handling data

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,19 +7,54 @@ use tauri::generate_context;
 use serde::{Serialize, Deserialize};
 use uuid::Uuid;
 
-#[derive(Serialize, Deserialize, Clone, Debug)]
+#[derive(Default, Serialize, Deserialize, Clone, Debug)]
 enum State {
     Loaded,
+    #[default]
     Loading
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 enum LoadErr {
     FileErr,
-    FormatErr
+}
+#[derive(Serialize, Deserialize, Clone, Debug)]
+
+enum SaveErr {
+    DirErr,
+    FileErr,
+    WriteErr,
+
+}
+
+#[derive(Default, Serialize, Deserialize, Clone, Debug)]
+enum AgoraMessage {
+    ConversationAdded(Conversation),
+    NameChanged(String, Uuid),
+    DescChanged(String, Uuid),
+    UserMessage(UserMessage),
+    InputChanged(String),
+    #[default]
+    CreateComment,
+    ConversationMessage(ConversationMessage)
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
+enum ConversationMessage {
+    CommentAdded(String),
+    CommentDeleted(Uuid),
+    UserAdded(User),
+    UserExited(Uuid)
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+enum UserMessage {
+    KindChange(String),
+    UserNameChange(String),
+    EmailChange(String),
+    PasswordChange(String)
+}
+#[derive(Default, Serialize, Deserialize, Clone, Debug)]
 
 struct AppState {
     loaded: State,
@@ -28,16 +63,39 @@ struct AppState {
     pub comments: Vec<Comment>
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug)]
-struct User {}
-#[derive(Serialize, Deserialize, Clone, Debug)]
-struct Agora {}
-#[derive(Serialize, Deserialize, Clone, Debug)]
+#[derive(Default, Serialize, Deserialize, Clone, Debug)]
+struct User {
+    kind: String,
+    user_name: String,
+    password: String,
+    conversations: Vec<Uuid>,
+    user_id: Uuid,
+    comments: Vec<String>
+}
+
+#[derive(Default, Serialize, Deserialize, Clone, Debug)]
+struct Conversation {
+    assembly: Vec<User>,
+    presenter: User,
+    comments: Vec<Comment>,
+    agora_id: Uuid
+}
+#[derive(Default, Serialize, Deserialize, Clone, Debug)]
+struct Agora {
+    name: String,
+    topic: String,
+    desc: String,
+    conversations: Vec<Conversation>,
+    founder: User
+}
+#[derive(Default, Serialize, Deserialize, Clone, Debug)]
 struct Comment {
     user_id : Uuid, // v4
     convo_id: Uuid, // v7
     content: String
 }
+
+
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
